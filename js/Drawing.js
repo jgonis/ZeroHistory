8ff66c910cdf9c924110b4a76b9236ea886c1680
Jeff Gonis <jeff.gonis@gmail.com>
2014-10-09 17:14:19 -0600

    trying to get canvas to resize properly

--- js/Drawing.js
+++ js/Drawing.js
@@ -1,23 +1,24 @@
 /**
-* Created by vgw44765 on 03/10/2014.
-*/
+ * Created by vgw44765 on 03/10/2014.
+ */
 window.onload = canvasResize;
 window.onresize = canvasResize;
-
 function canvasResize(event) {
+    console.log("Resizing!");
     var canvasContainer = document.getElementById("canvasContainer");
+    var footer = document.getElementById("canvasContainer");
     var width = canvasContainer.clientWidth;
     var height = canvasContainer.clientHeight;
     var canvas = document.getElementById("canvas");
     canvas.width = width;
-    canvas.height = height;
-    var context = canvas.getContext('2d');
-    context.strokeStyle = "rgb(255, 0, 0)";
-    context.lineWidth = 5;
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(context.canvas.width - 50, context.canvas.height - 50);
-    context.closePath();
-    context.stroke();
+    canvas.height = height - 10;
+    /*var context: CanvasRenderingContext2D = canvas.getContext('2d');
+     context.strokeStyle = "rgb(255, 0, 0)";
+     context.lineWidth = 5;
+     context.beginPath();
+     context.moveTo(0, 0);
+     context.lineTo(context.canvas.width - 50, context.canvas.height - 50);
+     context.closePath();
+     context.stroke();*/
 }
 //# sourceMappingURL=Drawing.js.map
--- js/Drawing.ts
+++ js/Drawing.ts
@@ -5,18 +5,20 @@ window.onload = canvasResize;
 window.onresize = canvasResize;
 
 function canvasResize(event) {
+    console.log("Resizing!");
     var canvasContainer = document.getElementById("canvasContainer");
+    var footer = document.getElementById("canvasContainer");
     var width = canvasContainer.clientWidth;
     var height = canvasContainer.clientHeight;
-    var canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas");
+    var canvas:HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas");
     canvas.width = width;
-    canvas.height = height;
-    var context: CanvasRenderingContext2D = canvas.getContext('2d');
-    context.strokeStyle = "rgb(255, 0, 0)";
-    context.lineWidth = 5;
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(context.canvas.width - 50, context.canvas.height - 50);
-    context.closePath();
-    context.stroke();
+    canvas.height = height - 10;
+    /*var context: CanvasRenderingContext2D = canvas.getContext('2d');
+     context.strokeStyle = "rgb(255, 0, 0)";
+     context.lineWidth = 5;
+     context.beginPath();
+     context.moveTo(0, 0);
+     context.lineTo(context.canvas.width - 50, context.canvas.height - 50);
+     context.closePath();
+     context.stroke();*/
 }
