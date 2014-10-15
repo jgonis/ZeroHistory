e0aa28b68fd306050e7e2785195bc561ecfb21ce
Jeff Gonis <jeff.gonis@gmail.com>
2014-10-15 16:33:05 -0600

    clean up a bunch of stuff and more flex changes
    Realize that I probably only need to resize canvas once on page load and
    not any other time. Go with this as a compromise for now.

--- js/HeatMap.js
+++
@@ -1,4 +0,0 @@
-/**
-* Created by vgw44765 on 03/10/2014.
-*/
-//# sourceMappingURL=HeatMap.js.map
--- js/RandomPainter.js
+++ js/RandomPainter.js
@@ -1,4 +1,39 @@
 /**
-* Created by vgw44765 on 03/10/2014.
-*/
+ * Created by vgw44765 on 03/10/2014.
+ */
+window.onload = canvasSetup;
+function draw(context, width, height) {
+    if (context != null) {
+        context.lineWidth = 5;
+        context.strokeStyle = "rgb(255, 0, 0)";
+        context.moveTo(0, height / 2);
+        context.lineTo(100, height / 2);
+        context.moveTo(width / 2, 0);
+        context.lineTo(width / 2, 100);
+        context.stroke();
+    }
+}
+function canvasResize(event) {
+    var canvas = (document.getElementById("canvas"));
+    var controlsOnLeft = document.getElementById("controls");
+    var controlsOnLeftWidth = controlsOnLeft.clientWidth;
+    var controlsBelow = document.getElementById("bottom");
+    var controlsBelowHeight = controlsBelow.clientHeight;
+    if (canvas != null) {
+        var canvasParent = canvas.parentElement;
+        if (canvasParent != null) {
+            canvas.width = canvasParent.clientWidth; // - (controlsOnLeftWidth);
+            canvas.height = canvasParent.clientHeight; // - (controlsBelowHeight);
+        }
+    }
+}
+function canvasSetup(event) {
+    var canvas = (document.getElementById("canvas"));
+    canvasResize(null);
+    if (canvas != null) {
+        var context = canvas.getContext("2d");
+        draw(context, canvas.width, canvas.height);
+    }
+    //window.onresize = canvasResize;
+}
 //# sourceMappingURL=RandomPainter.js.map
