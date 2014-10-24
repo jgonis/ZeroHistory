ac894711a7a76d33b1125c13994716f5d3680586
Jeff Gonis <jeff.gonis@gmail.com>
2014-10-24 16:35:50 -0600

    more work getting the random painter up and running

--- js/RandomPainter.js
+++ js/RandomPainter.js
@@ -1,7 +1,10 @@
 /**
  * Created by vgw44765 on 03/10/2014.
  */
-window.onload = function (event) {
+var rp = null;
+var continueProcessing = false;
+window.addEventListener('load', function (event) {
+    console.log("resizing canvas");
     var canvas = (document.getElementById("canvas"));
     if (canvas !== null) {
         var canvasParent = canvas.parentElement;
@@ -9,21 +12,75 @@ window.onload = function (event) {
             canvas.width = canvasParent.clientWidth;
             canvas.height = canvasParent.clientHeight;
         }
+        document.getElementById("startButton").addEventListener("click", function (mouseEvent) {
+            console.log("start button clicked");
+        });
+        document.getElementById("stopButton").addEventListener("click", function (mouseEvent) {
+            console.log("stop button clicked");
+        });
+        document.getElementById("resetButton").addEventListener("click", function (mouseEvent) {
+            console.log("reset button clicked");
+        });
+        document.getElementById("exportButton").addEventListener("click", function (mouseEvent) {
+            console.log("export button clicked");
+        });
+        rp = new RandomPaintCanvas(canvas.width, canvas.height, continueProcessing, canvas.getContext("2d"));
     }
-};
-function loopingFunc() {
-    var startTime = performance.now();
-    var iterationCount = 0;
-    var currentTime = 0;
-    var shouldLoop = true;
-    while (shouldLoop === true && this.continueProcessing === true) {
-        currentTime = performance.now();
-        if (currentTime - startTime >= 1000) {
-            this.postMessageFunc(iterationCount);
-            shouldLoop = false;
+});
+var RandomPaintCanvas = (function () {
+    function RandomPaintCanvas(width, height, continueProcessing, ctx) {
+        this.width = width;
+        this.height = height;
+        this.setContinueProcessing(continueProcessing);
+        this.ctx = ctx;
+    }
+    RandomPaintCanvas.prototype.loopingFunc = function () {
+        var startTime = performance.now();
+        var iterationCount = 0;
+        var currentTime = 0;
+        var shouldLoop = true;
+        while (shouldLoop === true && this.continueProcessing === true) {
+            currentTime = performance.now();
+            if (currentTime - startTime >= 1000) {
+                shouldLoop = false;
+            }
+            iterationCount = iterationCount + 1;
         }
-        iterationCount = iterationCount + 1;
+        window.setTimeout(this.loopingFunc, 0);
+    };
+    RandomPaintCanvas.prototype.setContinueProcessing = function (continueProcessing) {
+        this.continueProcessing = continueProcessing;
+    };
+    return RandomPaintCanvas;
+})();
+var RandomPainter = (function () {
+    function RandomPainter(x, y, maxX, maxY) {
+        this.x = x;
+        this.y = y;
+        this.maxX = maxX;
+        this.maxY = maxY;
     }
-    self.setTimeout(this.loopingFunc, 0);
-}
+    RandomPainter.prototype.getCurrentPosition = function () {
+        return { x: this.x, y: this.y };
+    };
+    RandomPainter.prototype.goToNextPosition = function () {
+        this.x = this.generateValidPosition(this.x, this.maxX);
+        this.y = this.generateValidPosition(this.y, this.maxY);
+    };
+    RandomPainter.prototype.generateValidPosition = function (currentPosition, positionMax) {
+        var validNextPosition = false;
+        while (validNextPosition === false) {
+            var positionOffset = Math.floor(Math.random() * (4 - (-3))) + (-3);
+            var tempPosition = currentPosition + positionOffset;
+            if (tempPosition < 0 || tempPosition > positionMax) {
+                validNextPosition = false;
+            }
+            else {
+                validNextPosition = true;
+                return tempPosition;
+            }
+        }
+    };
+    return RandomPainter;
+})();
 //# sourceMappingURL=RandomPainter.js.map
--- js/RandomPainter.ts
+++ js/RandomPainter.ts
@@ -1,7 +1,10 @@
 /**
  * Created by vgw44765 on 03/10/2014.
  */
-window.onload = function (event) {
+var rp: RandomPaintCanvas = null;
+var continueProcessing: boolean = false;
+window.addEventListener('load', (event: UIEvent ) => {
+    console.log("resizing canvas");
     var canvas = <HTMLCanvasElement>(document.getElementById("canvas"));
     if (canvas !== null) {
         var canvasParent = canvas.parentElement;
@@ -9,21 +12,95 @@ window.onload = function (event) {
             canvas.width = canvasParent.clientWidth;
             canvas.height = canvasParent.clientHeight;
         }
-    }
-};
+        document.getElementById("startButton").addEventListener("click", (mouseEvent: MouseEvent) => {
+            console.log("start button clicked");
+        });
 
-function loopingFunc() {
-    var startTime: number = performance.now();
-    var iterationCount: number = 0;
-    var currentTime: number = 0;
-    var shouldLoop: boolean = true;
-    while(shouldLoop === true && this.continueProcessing === true) {
-        currentTime = performance.now();
-        if(currentTime - startTime >= 1000) {
-            this.postMessageFunc(iterationCount);
-            shouldLoop = false;
-        }
-        iterationCount = iterationCount + 1;
+        document.getElementById("stopButton").addEventListener("click", (mouseEvent: MouseEvent) => {
+            console.log("stop button clicked");
+        });
+
+        document.getElementById("resetButton").addEventListener("click", (mouseEvent: MouseEvent) => {
+            console.log("reset button clicked");
+        });
+
+        document.getElementById("exportButton").addEventListener("click", (mouseEvent: MouseEvent) => {
+            console.log("export button clicked");
+        });
+
+        rp = new RandomPaintCanvas(canvas.width, canvas.height, continueProcessing, canvas.getContext("2d"));
     }
-    self.setTimeout(this.loopingFunc, 0);
+});
+
+class RandomPaintCanvas {
+    width: number;
+    height: number;
+    continueProcessing: boolean;
+    ctx: CanvasRenderingContext2D;
+    constructor(width: number, height: number, continueProcessing: boolean, ctx: CanvasRenderingContext2D) {
+        this.width = width;
+        this.height = height;
+        this.setContinueProcessing(continueProcessing);
+        this.ctx = ctx;
+    }
+
+    loopingFunc() {
+        var startTime: number = performance.now();
+        var iterationCount: number = 0;
+        var currentTime: number = 0;
+        var shouldLoop: boolean = true;
+        while(shouldLoop === true && this.continueProcessing === true) {
+            currentTime = performance.now();
+            if(currentTime - startTime >= 1000) {
+                shouldLoop = false;
+            }
+            iterationCount = iterationCount + 1;
+        }
+        window.setTimeout(this.loopingFunc, 0);
+    }
+
+    setContinueProcessing(continueProcessing: boolean ) {
+        this.continueProcessing = continueProcessing;
+    }
+}
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+class RandomPainter {
+    x: number;
+    y: number;
+    maxX: number;
+    maxY: number
+    constructor(x: number, y: number, maxX: number, maxY: number) {
+        this.x = x;
+        this.y = y;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+
+    getCurrentPosition() : Point {
+        return { x: this.x, y: this.y };
+    }
+
+    goToNextPosition() {
+        this.x = this.generateValidPosition(this.x, this.maxX);
+        this.y = this.generateValidPosition(this.y, this.maxY);
+    }
+
+    private generateValidPosition(currentPosition: number, positionMax: number) : number {
+        var validNextPosition = false;
+        while (validNextPosition === false) {
+            var positionOffset:number = Math.floor(Math.random() * (4 - (-3))) + (-3);
+            var tempPosition: number = currentPosition + positionOffset;
+            if (tempPosition < 0 || tempPosition > positionMax) {
+                validNextPosition = false;
+            } else {
+                validNextPosition = true;
+                return tempPosition;
+            }
+        }
+    }
 }
